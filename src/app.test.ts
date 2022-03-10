3552c7cb843961e4f63b2e13463ae396dd19da8f
Yusuke Wada <yusuke@kamawada.com>
2022-03-10 16:57:13 +0900

    use `request` method in test

--- src/app.test.ts
+++ src/app.test.ts
@@ -9,21 +9,19 @@ describe('basic', () => {
   })
 
   test('GET /', async () => {
-    const req = new Request('/', { method: 'GET' })
-    const res = await app.dispatch(req)
+    const res = await app.request('/', { method: 'GET' })
     expect(res.status).toBe(200)
   })
 
   test('Access control for a Bot', async () => {
     const req = new Request('/')
     req.headers.set('User-Agent', 'MalBot/0.01')
-    const res = await app.dispatch(req)
+    const res = await app.request(req)
     expect(res.status).toBe(403)
   })
 
   test('Unauthorized when using basic auth', async () => {
-    const req = new Request('/auth/aaa')
-    const res = await app.dispatch(req)
+    const res = await app.request('/auth/aaa')
     expect(res.status).toBe(401)
   })
 
@@ -31,7 +29,7 @@ describe('basic', () => {
     const credential = Buffer.from('compute:edge').toString('base64')
     const req = new Request('/auth/aaa')
     req.headers.set('Authorization', `Basic ${credential}`)
-    const res = await app.dispatch(req)
+    const res = await app.request(req)
     expect(res.status).toBe(200)
   })
 })
