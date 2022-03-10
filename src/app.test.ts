936c9e84db6a533b8e85845a4d6020f945fd2fdb
Yusuke Wada <yusuke@kamawada.com>
2022-03-10 14:45:22 +0900

    basic pattern (#1)

--- src/app.test.ts
+++ src/app.test.ts
@@ -6,10 +6,32 @@ describe('basic', () => {
   beforeEach(() => {
     Object.assign(global, makeServiceWorkerEnv())
     jest.resetModules()
-  }),
-    test('basic', async () => {
-      const req = new Request('/', { method: 'GET' })
-      const res = await app.dispatch(req)
-      expect(res.status).toBe(200)
-    })
+  })
+
+  test('GET /', async () => {
+    const req = new Request('/', { method: 'GET' })
+    const res = await app.dispatch(req)
+    expect(res.status).toBe(200)
+  })
+
+  test('Access control for a Bot', async () => {
+    const req = new Request('/')
+    req.headers.set('User-Agent', 'MalBot/0.01')
+    const res = await app.dispatch(req)
+    expect(res.status).toBe(403)
+  })
+
+  test('Unauthorized when using basic auth', async () => {
+    const req = new Request('/auth/aaa')
+    const res = await app.dispatch(req)
+    expect(res.status).toBe(401)
+  })
+
+  test('Authorized when using basic auth', async () => {
+    const credential = Buffer.from('compute:edge').toString('base64')
+    const req = new Request('/auth/aaa')
+    req.headers.set('Authorization', `Basic ${credential}`)
+    const res = await app.dispatch(req)
+    expect(res.status).toBe(200)
+  })
 })
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,20 @@ import { basicAuth } from 'hono/basic-auth'
 
 export const app = new Hono()
 
+/*
+if (req.http.User-Agent ~ "MalBot") {
+  error 403 "Forbidden";
+}
+*/
+app.use('*', async (c, next) => {
+  const ua = c.req.header('User-Agent') || ''
+  await next()
+  if (ua.match(/MalBot/)) {
+    c.res = c.text('Forbidden', 403)
+  }
+})
+
+/* Basic Auth */
 app.use(
   '/auth/*',
   basicAuth({
@@ -12,9 +26,9 @@ app.use(
     hashFunction: (m: string) => SHA256(m).toString(),
   })
 )
+app.get('/auth/*', (c) => c.text('You are authorized!'))
 
 app.get('/', (c) => c.text('Hello! Compute@Edge!'))
-app.get('/auth/*', (c) => c.text('You are authorized!'))
 app.get('/hello/:name', (c) => {
   return c.text(`Hello ${c.req.param('name')}!!`)
 })
